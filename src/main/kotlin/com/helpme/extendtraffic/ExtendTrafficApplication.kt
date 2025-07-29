9630d59f133fcf0c28da2a6404645e6873f83801
ass3027 <blackfeather227@gmail.com>
2025-07-30 01:07:28 +0900

    initial commit

---
+++ src/main/kotlin/com/helpme/extendtraffic/ExtendTrafficApplication.kt
@@ -0,0 +1,11 @@
+package com.helpme.extendtraffic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExtendTrafficApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExtendTrafficApplication>(*args)
+}
---
+++ src/test/kotlin/com/helpme/extendtraffic/ExtendTrafficApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.helpme.extendtraffic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExtendTrafficApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
